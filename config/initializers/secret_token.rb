64b72d5b447e4e4666b0ae3051c2b55aa0af1b6e
bcaputcu <berk.caputcu@jadedpixel.com>
2013-12-07 11:26:51 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LlcJobboard::Application.config.secret_key_base = '90f765547ffd90187e7160b632a5e669144525dbda875450e3edd39e3f69872e01f3f57414bd34c91ecd7f47a7bd06d2b8f9efa5441a900a58dbbcb1420e0a16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LlcJobboard::Application.config.session_store :cookie_store, key: '_llc-jobboard_session'
