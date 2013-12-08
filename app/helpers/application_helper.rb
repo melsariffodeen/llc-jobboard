5dff5ec1fb41d045cc18a982a76281345295f58b
Thomas Martineau <mart0657@gmail.com>
2013-12-07 20:37:50 -0500

    Added dynamic content column class and sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def admin_area?
     request.fullpath.split('/')[1] == 'admin' && admin_signed_in?
   end
+
+  def content_col_class
+    if admin_area?
+        'col-sm-12'
+    else
+        'col-sm-9'
+    end
+  end
 end
