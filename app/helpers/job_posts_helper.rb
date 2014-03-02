65d72602e2f20e9b8a2b02bc054181fe83b39608
bcaputcu <berk.caputcu@jadedpixel.com>
2014-03-02 10:34:04 -0500

    better expiry info

--- app/helpers/job_posts_helper.rb
+++ app/helpers/job_posts_helper.rb
@@ -1,2 +1,9 @@
 module JobPostsHelper
+  def expiry_info(job_post)
+    if job_post.expires_in > 0
+      "Active for another #{pluralize(job_post.expires_in, "day")}"
+    else
+      "Will expire today"
+    end
+  end
 end
