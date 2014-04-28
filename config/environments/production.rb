aab0e8eebbb52f6fd01b854f806ad6641529f693
bcaputcu <berk.caputcu@jadedpixel.com>
2014-04-27 19:39:28 -0500

    set default mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ LlcJobboard::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'llc-jobs.herokuapp.com' }
 end
